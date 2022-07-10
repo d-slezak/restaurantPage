31cb08d82702144ad72f038dc8fc2363a0bb643c
d.slezak <Slezak.Dan@outlook.com>
2022-07-09 21:51:33 -0400

    Remove homePage from index.html, Add home.js and imported into index.js

---
+++ src/home.js
@@ -0,0 +1,36 @@
+function renderHome() {
+    const homePage = document.createElement('div');
+    homePage.id = 'homePage';
+
+    const callToAction = document.createElement('div');
+    callToAction.classList.add('callToAction');
+
+    const welcome = document.createElement('h2');
+    welcome.id = 'welcome';
+    welcome.textContent = "Welcome to";
+    callToAction.appendChild(welcome);
+
+    const nameHeader = document.createElement('h2');
+    nameHeader.id = 'name';
+    nameHeader.textContent = 'Culbert\'s Bakery';
+    callToAction.appendChild(nameHeader);
+
+    const tagLine = document.createElement('h3');
+    tagLine.id = 'tagLine';
+    tagLine.textContent = 'Home of the Famous Cream Puff';
+    callToAction.appendChild(tagLine);
+
+    const creamPuff = document.createElement('img');
+    creamPuff.src="./images/culberts-cream-puffs.jpeg";
+    creamPuff.alt = "Cream Puff Donuts";
+    creamPuff.id = "creamPuff";
+    callToAction.appendChild(creamPuff);
+
+    homePage.appendChild(callToAction);
+
+    console.log('HEL?');
+    
+    return homePage;
+}
+
+export {renderHome};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { renderNav } from "./navBar";
+import { renderHome } from "./home";
 
 const content = document.getElementById('content');
 
 function homePage() {
     content.innerHTML= '';
     content.appendChild(renderNav());
+    content.appendChild(renderHome());
 }
 
 homePage();
